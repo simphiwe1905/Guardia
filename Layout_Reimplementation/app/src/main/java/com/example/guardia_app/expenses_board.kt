f9273f8698e1c3bfd1f02c2d61de097b5fc270df
Sisa <ST10454918@rcconnect.edu.za>
2026-05-28 01:41:13 +0200

    2 ed. Layout upload and file organisation
    I've moved the previous original edition of the app into Ed. 1.1. and uploaded the new reimplementation of the layout which matches the figma design, the redesign includes a few assets and will be completed with the other pages soon.

---
+++ Layout_Reimplementation/app/src/main/java/com/example/guardia_app/expenses_board.kt
@@ -0,0 +1,20 @@
+package com.example.guardia_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class expenses_board : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_expenses_board)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
